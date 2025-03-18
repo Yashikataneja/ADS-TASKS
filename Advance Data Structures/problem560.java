90aed552b353618a99951f4b5075afa5949626c1
Yashikataneja <tanejayesha458@gmail.com>
2025-03-18 12:36:20 +0530

    Add files via upload

--- Advance Data Structures/problem560.java
+++ Advance Data Structures/problem560.java
@@ -1,28 +1,22 @@
-
+import java.util.HashMap;
+import java.util.Map;
 public class problem560 {
-    public int subarraySum(int[] nums, int k) {
-        int count = 0;
-        // Iterate over all possible subarrays
-        for (int i = 0; i < nums.length; i++) {
-            int sum = 0;
-            // Sum up elements for each subarray starting at index i
-            for (int j = i; j < nums.length; j++) {
-                sum += nums[j];
-                // Check if the sum is equal to k
-                if (sum == k) {
-                    count++;
+        public static int subarraysum(int[]nums,int k){
+            Map<Integer,Integer>pSC=new HashMap<>();
+            pSC.put(0,1);
+            int preSum=0,co=0;
+            for(int num:nums){
+                preSum+=num;
+                if(pSC.containsKey(preSum-k)){
+                    co+=pSC.get(preSum-k);
                 }
+                pSC.put(preSum,pSC.getOrDefault(preSum,0)+1);
             }
+            return co;
         }
-        return count;
-    }
-    public static void main(String[] args) {
-        problem560 solution = new problem560();
-        int[] nums1 = {1, 1, 1};
-        int k1 = 2;
-        System.out.println("Number of subarrays for example 1: " + solution.subarraySum(nums1, k1));  
-        int[] nums2 = {1, 2, 3};
-        int k2 = 3;
-        System.out.println("Number of subarrays for example 2: " + solution.subarraySum(nums2, k2)); 
-    }
+        public static void main(String[] args) {
+            int[]nums={1,1,1};
+            int k=2;
+            System.out.println(subarraysum(nums,k));
+        }
 }
