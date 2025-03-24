660d7d9b68f8865c0117c39f07b236e3f07b1c77
Yashikataneja <tanejayesha458@gmail.com>
2025-03-24 22:18:49 +0530

    Add files via upload

---
+++ Advance Data Structures/leetcode503.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class leetcode503 {
+    public int[] nextGreaterElements(int[] nums) {
+      final int n = nums.length;
+      int[] ans = new int[n];
+      Arrays.fill(ans, -1);
+      Deque<Integer> stack = new ArrayDeque<>(); // a decreasing stack storing indices
+  
+      for (int i = 0; i < n * 2; ++i) {
+        final int num = nums[i % n];
+        while (!stack.isEmpty() && nums[stack.peek()] < num)
+          ans[stack.pop()] = num;
+        if (i < n)
+          stack.push(i);
+      }
+  
+      return ans;
+    }
+  }
